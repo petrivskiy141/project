0ce397ea163a169fe93d545a875cf9190a16c976
Yaroslaw <petrowskiy141@gmail.com>
2021-10-15 21:33:23 +0300

    if.else

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
- const isChecked = true;
-        isClose = true;
+const num = 50 ;
 
+switch (num) {
+       case 49:
+              console.log(`Pizdes`);
+              break;
+       case 100:
+              console.log(`Pizdes`);
+              break;
+       case 440:
+              console.log(`pravda`);
+              break;
+       default:
+              console.log(`ne ne bratan`);
+              break;
 
+}
