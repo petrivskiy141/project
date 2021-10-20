179876c3d608deeafce2213b5af4aea6f7a26edb
Yaroslaw <petrowskiy141@gmail.com>
2021-10-20 21:14:48 +0300

    math

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const str = 'teSt';
+
+console.log (str.toLocaleLowerCase());
+console.log(str);
+
+let fruit = "some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.5;
+console.log(Math.round(num));
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
