d03bc733d9bd01cd724d6363b22d0daffce98290
Yaroslaw <petrowskiy141@gmail.com>
2021-11-01 07:58:13 +0200

    new test

--- js/script.js
+++ js/script.js
@@ -1,39 +1,6 @@
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log("test");
-    }
-};
+const p = document.querySelectorAll('p');
+console.log(p);
 
-options.makeTest();
-
-const {border, bg} = options.colors;
-console.log(bg);
-
-// console.log(Object.keys(options).length);
-
-// console.log(options.name);
-
-// delete options.name;
-
-// // console.log(options);
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) == 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеэт значениу ${options[key][i]}`);
-            
-//         }
-//     } else {console.log(`Свойство ${key} имеэт значениу ${options[key]}`);
-//     counter++;
-
-//     }
-    
-// }
-// console.log(counter);
+const script = document.createElement('script');
+script.src = "js/test.js";
+document.body.append(script);
